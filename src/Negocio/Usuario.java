73a53ccb3ad13e6bd69b1a9215c62c7e96ed5593
Fede <fpotilinskigmail.com>
2018-05-27 17:43:53 -0300

    Prueba
    Prueba

--- src/Negocio/Usuario.java
+++ src/Negocio/Usuario.java
@@ -1,5 +1,12 @@
 package Negocio;
 
 public class Usuario {
-
+	private String email;
+	private String password;
+	private String nombre;
+	private String apellido;
+	private String tipoDocumento;
+	private String nroDocumento;
+	private Direccion direccion;
+	private String test;
 }
